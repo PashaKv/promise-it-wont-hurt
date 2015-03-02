02465f2244dbe1fc401b93d4a23068287ecb2b8a
PashaKv <pasha.templar@gmail.com>
2015-03-02 22:40:40 +0200

    9th task bonuses

---
+++ l9superbonus.js
@@ -0,0 +1,10 @@
+var q = require('q');
+
+var d1 = q.defer(), d2 = q.defer();
+
+q.all([d1.promise, d2.promise]).spread(console.log).then(null, console.log);
+
+setTimeout(function(){
+	d1.resolve('PROMISES');
+	d2.resolve('FTW');
+}, 200);
